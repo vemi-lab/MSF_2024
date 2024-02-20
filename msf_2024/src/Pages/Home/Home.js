65687446e3cf8a559850b8d584c6ebf6b7665655
Anthony Caccese <anthony.m.caccese@gmail.com>
2024-02-20 17:11:09 -0500

    Modal and other things running

---
+++ msf_2024/src/Pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import vemiLogo from '../../assets/VEMI-Logo-2020-1024x430.png';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+export default function Home() {    
+
+    function startExperience(){
+        console.log("Starting experience");
+        // navigate to the next page
+
+    }
+    return (
+        <div>
+            <img src={vemiLogo} alt="VEMI Logo" />
+            <h2>Welcome to Duck Chase!</h2>
+            <img src="" alt="Duck chase logo or something?" />
+            <p>Search around the area for hiddens <b>codes</b> gain points and win a <b>prize</b>!</p>
+            <Link to="/game">Start</Link>
+        </div>
+    );
+}
