43aac5d00c35179476db545bf52c61a3fc240296
Anthony Caccese <anthony.m.caccese@gmail.com>
2024-03-06 11:31:30 -0500

    Added a game completion screen

--- msf_2024/src/Pages/Game/Game.js
+++ msf_2024/src/Pages/Game/Game.js
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DemoButton from '../../components/DemoButton';
 
-export default function Game() {    
+export default function Game() {
     const location = useLocation();
     const [completedPages, setCompletedPages] = useState({});
+    const [showEndScreen, setShowEndScreen] = useState(false);
     const totalQuestions = 6;
 
     useEffect(() => {
         if (location.state && location.state.userData) {
             checkForCompletedPage();
         }
+        checkForEndOfGame();
     }, [location]);
 
     function checkForCompletedPage() {
@@ -22,13 +24,20 @@ export default function Game() {
         setCompletedPages(completed);
     }
 
+    function checkForEndOfGame(){
+        const numCompletedDemos= location.state !=null ? location.state.userData.completedDemos.length : 0;
+        if(numCompletedDemos == pageIDs.length){
+            setShowEndScreen(true);
+        }
+    }
     const pageIDs = ["/AVADemo", "/HowToResearch", "/SimInfo"];
 
     return (
         <div>
-            <h1>Score</h1>
-            <h2>{location.state!=null ? (location.state.userData.score/totalQuestions *100).toPrecision(3) : 0}%</h2>
-            {pageIDs.map((id, index) => (
+            {( showEndScreen ? <h1>Congratulations</h1> : <h1>Score</h1>)}
+            {( showEndScreen ? <h2>Final Score {location.state!=null ? (location.state.userData.score/totalQuestions * 100).toPrecision(3) : 0}%</h2>: <h2>{location.state!=null ? (location.state.userData.score/totalQuestions * 100).toPrecision(3) : 0}%</h2>)}
+            
+            {( showEndScreen ? <></>: pageIDs.map((id, index) => (
                 <DemoButton
                     key={index}
                     userData={location.state!=null ? location.state.userData : null}
@@ -37,7 +46,8 @@ export default function Game() {
                     code={id === "/AVADemo" ? "15070" : id === "/HowToResearch" ? "68978" : "74343"}
                     isCompleted={completedPages[id] || false}
                 />
-            ))}
+            )))}
+
         </div>
     );
 }
