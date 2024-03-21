9ee3310afbe4b2d955aea153a3f5c1351c6c1c47
Anthony Caccese <anthony.m.caccese@gmail.com>
2024-03-21 12:21:30 -0400

    Updated SimInfo Questions

--- msf_2024/src/Pages/SimInfo/SimInfo.js
+++ msf_2024/src/Pages/SimInfo/SimInfo.js
@@ -8,19 +8,35 @@ export default function SimInfo() {
     const [toggleQuestions, setQuestions] = useState(false);
     const questions = [
         {
-            text: "What do you think?",
+            text: "What is a fully autonomous vehicle simulator like?",
             options: [
-                {id: 0, text: "Answer 1", isCorrect: false},
-                {id: 1, text: "Answer 2", isCorrect: false},
-                {id: 2, text: "Answer 3", isCorrect: true}
+                {id: 0, text: "It's like a big, awesome car game", isCorrect: true},
+                {id: 1, text: "It's like a magical playset with screens and computers", isCorrect: false},
+                {id: 2, text: "It's like a giant book with pictures", isCorrect: false}
             ]
         },
         {
-            text: "IDK?",
+            text: "What makes autonomous vehicles special?",
             options: [
-                {id: 0, text: "Answer 1", isCorrect: false},
-                {id: 1, text: "Answer 2", isCorrect: true},
-                {id: 2, text: "Answer 3", isCorrect: false}
+                {id: 0, text: "They have wings and can fly", isCorrect: false},
+                {id: 1, text: "They can drive themselves", isCorrect: true},
+                {id: 2, text: "They can talk to animals", isCorrect: false}
+            ]
+        },
+        {
+            text: "How do autonomous vehicles \"see\" the world around them?",
+            options: [
+                {id: 0, text: "They use super smart sensors that act like eyes", isCorrect: true},
+                {id: 1, text: "They use magic spells", isCorrect: false},
+                {id: 2, text: "hey close their eyes and guess", isCorrect: false}
+            ]
+        },
+        {
+            text: "How do autonomous vehicles help make roads safer?",
+            options: [
+                {id: 0, text: "By causing accidents", isCorrect: false},
+                {id: 1, text: "By driving really fast", isCorrect: false},
+                {id: 2, text: "By helping scientists and engineers learn how to make roads safer", isCorrect: true}
             ]
         },
     ];
