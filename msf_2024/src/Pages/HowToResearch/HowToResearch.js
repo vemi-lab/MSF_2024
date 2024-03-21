c3b9ee5859a6a5061ff9cd595633e30f14356a59
Anthony Caccese <anthony.m.caccese@gmail.com>
2024-03-21 12:14:49 -0400

    Updated the questions HowToResearch

--- msf_2024/src/Pages/HowToResearch/HowToResearch.js
+++ msf_2024/src/Pages/HowToResearch/HowToResearch.js
@@ -14,19 +14,35 @@ export default function HowToResearch() {
     const [toggleQuestions, setQuestions] = useState(false);
     const questions = [
         {
-            text: "What do you think?",
+            text: "What is the scientific method used for?",
             options: [
-                {id: 0, text: "Answer 1", isCorrect: false},
-                {id: 1, text: "Answer 2", isCorrect: false},
-                {id: 2, text: "Answer 3", isCorrect: true}
+                {id: 0, text: "Discovering answers to new questions", isCorrect: true},
+                {id: 1, text: "Making cookies", isCorrect: false},
+                {id: 2, text: "Playing games", isCorrect: false}
             ]
         },
         {
-            text: "IDK?",
+            text: "What is a hypothesis?",
             options: [
-                {id: 0, text: "Answer 1", isCorrect: false},
-                {id: 1, text: "Answer 2", isCorrect: true},
-                {id: 2, text: "Answer 3", isCorrect: false}
+                {id: 0, text: "A type of flower", isCorrect: false},
+                {id: 1, text: "An educated guess to answer a question", isCorrect: true},
+                {id: 2, text: "A type of bird", isCorrect: false}
+            ]
+        },
+        {
+            text: "What is the independent variable in the ice melting experiment?",
+            options: [
+                {id: 0, text: "The type of ice used", isCorrect: false},
+                {id: 1, text: "The time it takes for the ice to melt", isCorrect: false},
+                {id: 2, text: "Whether the water has salt or no salt", isCorrect: true}
+            ]
+        },
+        {
+            text: "How do researchers present their conclusions?",
+            options: [
+                {id: 0, text: "By writing papers and giving presentations", isCorrect: true},
+                {id: 1, text: "By making sandwiches", isCorrect: false},
+                {id: 2, text: "By singing songs", isCorrect: false}
             ]
         },
     ];
